7fb17d67b2b42b04225b1ebf3c58a0340552ae44
ankur0904 <ankursingh91002@gmail.com>
2024-02-17 23:53:08 +0530

    Made mobile responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,19 @@ import logo from '../ankur.png';
 function Hero() {
     return (
         <section className="py-5 text-center container">
-            <div className="row py-lg-5">
+            <div className="row py-lg-4">
                 <div className="col-lg-6 col-md-8 mx-auto">
                    <img className='profile-photo' src={logo} />
                 </div>
                 
             </div>
-            <div className="row py-lg-5">
-                <div className="col-lg-6 col-md-8 mx-auto">
+            <div className="row py-lg-4">
+                <div className="col-lg-6 col-md-8 mx-auto hero-text">
                     <h1 className="fw-light">Hi, I am Ankur Singh...</h1>
                     <p className="lead text-body-secondary">A passinate web developer, Devops practictioner & open source contributor.</p>
                     <p>
-                        <a href="#" className="btn btn-primary my-2">Main call to action</a>
-                        <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+                        <a href="#" className="btn btn-primary my-2 hero-button">Download Resume</a>
+                        <a href="#" className="btn btn-secondary my-2">Contact me</a>
                     </p>
                 </div>
                 
