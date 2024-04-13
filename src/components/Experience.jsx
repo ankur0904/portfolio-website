d4939b12a40f38e7669ef73336ddd5cb48b045cc
ankur0904 <ankursingh91002@gmail.com>
2024-04-14 00:04:58 +0530

    Finalized experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -28,10 +28,10 @@ function Experience() {
                 <a href="#" className="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
                     <div className="d-flex gap-2 w-100 justify-content-between">
                         <div>
-                            <h6 className="mb-0">Third heading</h6>
-                            <p className="mb-0 opacity-75">Some placeholder content in a paragraph.</p>
+                            <h6 className="mb-0">OSS contributor</h6>
+                            <p className="mb-0 opacity-75">Contributed to Numpy, K8s and other popular open source projects.</p>
                         </div>
-                        <small className="opacity-50 text-nowrap">1w</small>
+                        <small className="opacity-50 text-nowrap">Part time</small>
                     </div>
                 </a>
             </div>
