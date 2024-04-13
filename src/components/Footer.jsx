9ad69e2c68ddbf858baabd28f51db009245f9e2d
ankur0904 <ankursingh91002@gmail.com>
2024-04-14 00:02:07 +0530

    Adds footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+    return (
+        <div class="container">
+            <footer class="py-3 mt-5">
+                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item"><a href="https://github.com/ankur0904/" class="nav-link px-2 text-body-secondary">Github</a></li>
+                    <li class="nav-item"><a href="https://www.linkedin.com/in/ankur-singh-161458227/" class="nav-link px-2 text-body-secondary">LinkedIn</a></li>
+                    <li class="nav-item"><a href="https://twitter.com/ankur_136" class="nav-link px-2 text-body-secondary">Twitter</a></li>
+                    <li class="nav-item"><a href="https://leetcode.com/ankursingh91002/" class="nav-link px-2 text-body-secondary">Leetcode</a></li>
+                    <li class="nav-item"><a href="mailto:ankursingh91002@gmail.com" class="nav-link px-2 text-body-secondary">Email</a></li>
+                </ul>
+                {/* <p class="text-center text-body-secondary">© 2024 Company, Inc</p> */}
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
